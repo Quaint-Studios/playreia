f456301fda5cb3e82e33ccad0bbffc51fb6496e7
Makosai <kristopher.ali.dev@gmail.com>
2023-12-16 16:09:11 -0500

    Update the hero section.

--- src/routes/components/Hero.svelte
+++ src/routes/components/Hero.svelte
@@ -7,29 +7,22 @@
 	import WindowsChip from './platforms/WindowsChip.svelte';
 	import XboxChip from './platforms/XboxChip.svelte';
 
-  import Hero_1 from '$lib/assets/hero_1.jpg?enhanced';
-
-	const chipFlex = 'mt-auto w-full max-w-[100vw] mb-1 grid grid-flow-col justify-center gap-10';
+	const chipFlex = '';
 </script>
 
-<div class="relative w-full h-max">
-	<div>
-		<div class="dither"></div>
-		<enhanced:img src="$lib/assets/hero_1.jpg" alt="Temp Game Background" class="relative z-[-2] w-full h-4/5 object-cover" />
-
-	</div>
-	<div class="absolute w-full h-full flex justify-center items-end pb-10 bottom-0">
-		<div class="flex flex-col gap-1">
-			<div class={chipFlex}>
+<div id="hero-section">
+	<div id="hero-content">
+		<div class="flex flex-col gap-1 scale-[0.4] sm:scale-[0.6] md:scale-[0.70] lg:scale-75">
+			<div class="chip-flex">
 				<WindowsChip />
 				<MacChip />
 			</div>
-			<div class={chipFlex}>
+			<div class="chip-flex">
 				<XboxChip />
 				<PsChip />
 				<SwitchChip />
 			</div>
-			<div class={chipFlex}>
+			<div class="chip-flex">
 				<AppStoreChip />
 				<PlayStoreChip />
 			</div>
@@ -38,12 +31,16 @@
 </div>
 
 <style>
-	.dither {
-    @apply absolute left-0 top-0 w-full h-36 z-[-1];
-		background-image: linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0));
+	#hero-section {
+		@apply relative w-full h-4/5 min-h-[800px] bg-cover bg-center bg-no-repeat;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.0)), url("/rsc/img/hero_1.jpg?enhanced")
 	}
-  .heroBG {
-    @apply w-full h-4/5 bg-cover;
-    background-image: Hero_1
-  }
+
+	#hero-content {
+		@apply relative w-full h-full flex justify-center items-end pb-10;
+	}
+
+.chip-flex {
+  @apply mt-auto w-full max-w-[100vw] mb-1 grid grid-flow-col justify-center gap-10;
+}
 </style>
