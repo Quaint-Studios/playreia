a31b95d8f292438fecfc8b7f949cd19621c98c20
Makosai <kristopher.ali.dev@gmail.com>
2023-12-18 06:11:38 -0500

    Improve and further implement the hero content.

--- src/routes/components/hero/Hero.svelte
+++ src/routes/components/hero/Hero.svelte
@@ -1,10 +1,12 @@
 <script>
+	import HeroContent from './HeroContent.svelte';
 	import HeroPlatformChips from './HeroPlatformChips.svelte';
 </script>
 
 <div id="hero-section">
 	<div id="hero-wrapper">
-		<div class="flex flex-col gap-1 items-center">
+		<HeroContent />
+		<div class="w-full flex flex-col gap-1 items-center">
 			<div class="text-lg font-bold mb-4">
 				<a href="/download">Coming soon to all platforms - Play now!</a>
 			</div>
@@ -15,12 +17,12 @@
 
 <style>
 	#hero-section {
-		@apply relative w-full h-4/5 min-h-[800px] bg-cover bg-center bg-no-repeat;
+		@apply relative w-full h-4/5 min-h-[740px] flex justify-center items-end bg-cover bg-center bg-no-repeat;
 		background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0)),
 			url('/rsc/img/hero_1.jpg?enhanced');
 	}
 
 	#hero-wrapper {
-		@apply relative w-full h-full flex justify-center items-end pb-10;
+		@apply relative flex w-full h-full shrink flex-col justify-between content-between items-center px-10 pt-48 pb-10;
 	}
 </style>
--- src/routes/components/hero/HeroContent.svelte
+++ src/routes/components/hero/HeroContent.svelte
@@ -1,14 +1,29 @@
 <script>
-
 </script>
 
 <div class="hero-content">
-<h2>Explore endless magical worlds!</h2>
-<h3>Reia is an action-RPG. In this game you can make the world yours.</h3>
+	<div class="hero-content-wrapper">
+    <h2>Explore endless magical worlds!</h2>
+    <h3>Reia is an action-adventure RPG and also an open-source game. Play offline or online with friends, or login for an MMO experience! Create and explore worlds, manage your own economy and products, and restore Reia's world via the story. Choose the way you want to play.</h3>
+		<!--<h2>Explore endless magical worlds!</h2>
+		<h3>Reia is an action-RPG. In this game you can make the world yours.</h3>-->
+	</div>
 </div>
 
 <style>
-.hero-content {
-  @apply w-full h-full;
-}
+	.hero-content {
+		@apply w-full flex justify-start;
+	}
+
+	.hero-content-wrapper {
+		@apply max-w-[370px] sm:max-w-[390px] md:max-w-[526px] flex flex-col gap-4 sm:gap-6 md:gap-8;
+	}
+
+	.hero-content h2 {
+		@apply block text-4xl sm:text-4xl md:text-5xl lg:text-5xl font-bold;
+	}
+
+	.hero-content h3 {
+		@apply block font-medium;
+	}
 </style>
