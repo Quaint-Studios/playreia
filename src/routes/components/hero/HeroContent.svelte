fb0665614594ae4039e61918166f9eac6814e44b
Makosai <kristopher.ali.dev@gmail.com>
2023-12-18 00:43:03 -0500

    SEO and SSR implementations.

--- src/routes/components/hero/HeroContent.svelte
+++ src/routes/components/hero/HeroContent.svelte
@@ -3,7 +3,8 @@
 </script>
 
 <div class="hero-content">
-
+<h2>Explore endless magical worlds!</h2>
+<h3>Reia is an action-RPG. In this game you can make the world yours.</h3>
 </div>
 
 <style>
---
+++ src/routes/meta.svelte
@@ -0,0 +1,24 @@
+<script>
+	/** @type {string} */
+	export let title;
+	export let description =
+		"Explore endless worlds and embark on a magical adventure of a lifetime! Reia is an action-adventure RPG and also an open-source game. Play offline or online with friends, or login for an MMO experience! Create and explore worlds, manage your own economy and products, and restore Reia's world via the story. Choose the way you want to play.";
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<!-- <link rel="canonical" href="https://playreia.com" /> -->
+	<meta name="title" content={title} />
+	<meta name="description" content={description} />
+	<meta
+		name="keywords"
+		content="Reia, riea, reia 2023, reia game, rhea game, rhea, reia video, reia godot, reia godot 4, reia godot4, reia Android, reia Apple, reia iOS, reia iPhone, reia mobile, reia PC, reia Desktop, reia Console, reia Playstation, reia PS4, reia PS5, reia Microsoft, reia XBOX, reia register, reia pre-register, reia beautiful, Reia indie, reia good, reia graphics, reia launch, play reia, playreia, reia test, reia release, reia registration, reia review, reia video, game, action, adventure, 3d, open world, dungeons, story, magic, multiplayer, multi player, singleplayer, single player, online, offline, video-game, videogame, video game, indie, MMO, MMORPG, oRPG, godot, godot4, indie, windows, windows 10, mac, macos, macosx, linux, PC, Desktop, PS4, PS5, XBOX, Switch, Nintendo Switch, best mobile game,  best pc game, MMO PlayStation, MMORPG PS4, Action RPG, MMO game, multiple platforms, multiplayer game, multiplayer mobile game, action role-playing game, indie game, indie company, small indie company, listen to feedback, community, open-source game, code, github, github game"
+	/>
+	<meta name="robots" content="index, follow" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="language" content="English" />
+	<meta name="revisit-after" content="3 days" />
+	<meta name="author" content="Quaint Studios" />
+	<slot name="structured" />
+	<slot />
+</svelte:head>
