21a991150d25083f5be2fac6841163b2084c7e40
Makosai <kristopher.ali.dev@gmail.com>
2023-12-18 06:11:23 -0500

    Import more font weights for Poppins.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,15 +12,15 @@
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
 	/*
-	import '@fontsource/poppins/100.css';
+	import '@fontsource/poppins/100.css'; */
 	import '@fontsource/poppins/200.css';
-	import '@fontsource/poppins/300.css'; */
+	import '@fontsource/poppins/300.css';
 	import '@fontsource/poppins/400.css';
 	/*import '@fontsource/poppins/500.css';
-	import '@fontsource/poppins/600.css';
+	import '@fontsource/poppins/600.css'; */
 	import '@fontsource/poppins/700.css';
-	import '@fontsource/poppins/800.css';
-	import '@fontsource/poppins/900.css'; */
+	/* import '@fontsource/poppins/800.css'; */
+	import '@fontsource/poppins/900.css';
 	/*
 	import '@fontsource/poppins/100-italic.css';
 	import '@fontsource/poppins/200-italic.css';
