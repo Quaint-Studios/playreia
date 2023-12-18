04482106990dc5c39dc3b26fb60eb27b63137731
Makosai <kristopher.ali.dev@gmail.com>
2023-12-18 03:52:39 -0500

    Use a Vercel adapter.

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,27 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [ vitePreprocess()],
-	
+	preprocess: [vitePreprocess()],
+
 	vitePlugin: {
-		inspector: true,
+		inspector: true
 	},
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			images: {
+				sizes: [640, 828, 1200, 1920, 3840],
+				formats: ['image/avif', 'image/webp'],
+				minimumCacheTTL: 300
+			}
+		})
 	}
 };
 export default config;
