fc156f7c0afdd19c2172a3413872104138cbf478
Makosai <kristopher.ali.dev@gmail.com>
2023-12-18 23:53:46 -0500

    Create an Info section.

---
+++ src/routes/components/section/InfoContent.svelte
@@ -0,0 +1,17 @@
+<script>
+  export const title = "Content";
+</script>
+
+<div class="info-content">
+  <h2 class="title">{title}</h2>
+  <slot />
+</div>
+
+<style>
+	/* Content */
+	.info-content .title {
+		@apply text-primary-700 text-2xl md:text-3xl font-bold uppercase;
+	}
+	:global(.dark) .info-content .title {
+	}
+</style>
---
+++ src/routes/components/section/InfoNews.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { preview } from 'vite';
+
+	/**
+	 * @typedef {{
+	 * 'title': string,
+	 * 'image': string?,
+	 * 'imageAlt': string?,
+	 * 'imageTitle': string?,
+	 * 'url': string,
+	 * 'preview': string
+	 * }} NewsItem
+	 */
+
+	export const title = 'Content';
+	/**
+	 * @type {Array<NewsItem>}
+	 */
+	export let items = [];
+</script>
+
+<div class="info-news">
+	{#each items as item}
+		<div class="item">
+			<a href={item.url}>
+				<h2 class="title">{title}</h2>
+				<enhanced:img class="item-image" src={item.image} alt={item.imageAlt} title={item.imageTitle} />
+			</a>
+			<h3>{item.preview}</h3>
+		</div>
+	{/each}
+</div>
+
+<style>
+	/* Content */
+	.info-news .title {
+		@apply text-primary-700 text-2xl md:text-3xl font-bold uppercase;
+	}
+	:global(.dark) .info-content .title {
+	}
+
+  .item-image {
+    @apply object-cover w-3, h-1;
+  }
+</style>
