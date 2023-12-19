a3778ddedf11fc5ed55e1db062d53e25b0d28f73
Makosai <kristopher.ali.dev@gmail.com>
2023-12-18 23:54:33 -0500

    Implement the InfoSection.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,21 @@
 	import LoremIpsum from './components/filler/LoremIpsum.svelte';
 	import Hero from './components/hero/Hero.svelte';
 	import Meta from './components/head/meta.svelte';
+	import InfoSection from './components/section/InfoSection.svelte';
+	import InfoContent from './components/section/InfoContent.svelte';
+	import InfoNews from './components/section/InfoNews.svelte';
 </script>
 
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <div class="w-full h-full">
 	<Hero />
+	<InfoSection title="News">
+		<InfoNews slot="content">Hello world!</InfoNews>
+	</InfoSection>
+	<InfoSection title="Explore">
+
+  </InfoSection>
 	<LoremIpsum />
 </div>
 
