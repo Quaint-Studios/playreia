85395f371d6bd79a00b1bf4821432b6f44b29fc9
Makosai <kristopher.ali.dev@gmail.com>
2023-12-19 06:31:34 -0500

    Update HeroContent.svelte

--- src/routes/components/hero/HeroContent.svelte
+++ src/routes/components/hero/HeroContent.svelte
@@ -3,8 +3,12 @@
 
 <div class="hero-content">
 	<div class="hero-content-wrapper">
-    <h2>Explore endless magical worlds!</h2>
-    <h3>Reia is an action-adventure RPG and also an open-source game. Play offline or online with friends, or login for an MMO experience! Create and explore worlds, manage your own economy and products, and restore Reia's world via the story. Choose the way you want to play.</h3>
+		<h2>Explore endless magical worlds!</h2>
+		<h3>
+			Reia is an action-adventure RPG and also an open-source game. Play offline or online with
+			friends, or login for an MMO experience! Create and explore worlds, manage your own economy
+			and products, and restore Reia's world via the story. Choose the way you want to play.
+		</h3>
 		<!--<h2>Explore endless magical worlds!</h2>
 		<h3>Reia is an action-RPG. In this game you can make the world yours.</h3>-->
 	</div>
@@ -16,7 +20,7 @@
 	}
 
 	.hero-content-wrapper {
-		@apply max-w-[370px] sm:max-w-[390px] md:max-w-[526px] flex flex-col gap-4 sm:gap-6 md:gap-8;
+		@apply max-w-[370rpx] sm:max-w-[390px] md:max-w-[526px] flex flex-col gap-4 sm:gap-6 md:gap-8;
 	}
 
 	.hero-content h2 {
