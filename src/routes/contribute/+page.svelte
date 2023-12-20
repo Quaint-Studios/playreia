d9c865b2a881e3a39cd4d992d52bf83a501c0198
Makosai <kristopher.ali.dev@gmail.com>
2023-12-19 22:05:04 -0500

    Update the contribute page.

--- src/routes/contribute/+page.svelte
+++ src/routes/contribute/+page.svelte
@@ -2,7 +2,30 @@
 	import Meta from '../components/head/meta.svelte';
 </script>
 
-<Meta>
+<div class="mt-40 mb-20 mx-10">
+	<h1 class="h1 text-center font-bold">Contribute</h1>
+	<h2 id="#GitHub" class="h2"># GitHub</h2>
+	<p>
+		You can contribute via the GitHub repository <a
+			class="btn bg-primary-500 py-1"
+			href="https://github.playreia.com">here</a
+		>
+		<br /><br />
+		There are several tasks you can complete ranging from creating beautiful art, code, music, and more!
+		If you see a way you can move the project forward with us, join in.
+	</p>
+	<h2 id="#Funding" class="h2"># Funding</h2>
+	<p>
+		Another way you can contribute to this project is through funding. And if there's something
+		specific that we are already working on and you'd like us to give it some more focus, you can
+		emphasize that by voicing your concern. Currently we're deciding the best platform to accept
+		funding long-term.
+
+    Currently we're thinking of <a href="https://opencollective.com/reia">Open Collective</a>. But that may change depending on some factors; fees being one of them.
+	</p>
+</div>
+
+<Meta title="Contribute to the project!">
 	<script type="application/ld+json" slot="structured">
     {
       "@context": "https://schema.org/",
@@ -12,3 +35,9 @@
     }
 	</script>
 </Meta>
+
+<style>
+  h2 {
+    @apply mt-8;
+  }
+</style>
