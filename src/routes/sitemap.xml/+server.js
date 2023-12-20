bcaf015a253af78da93c85fc0e3795fb5f7ca319
Makosai <kristopher.ali.dev@gmail.com>
2023-12-20 02:49:51 -0500

    Update +server.js

--- src/routes/sitemap.xml/+server.js
+++ src/routes/sitemap.xml/+server.js
@@ -77,20 +77,16 @@ export async function GET() {
 	];
 	const date = '2023-12-20';
 
-	return new Response(
-		`
-		<?xml version="1.0" encoding="UTF-8" ?>
-		<urlset
-			xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-			xmlns:xhtml="https://www.w3.org/1999/xhtml"
-			xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
-			xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
-			xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
-			xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
-		>
-  ${pageData
-		.map(
-			(elem) => `
+	return new Response(`<?xml version="1.0" encoding="UTF-8" ?>
+<urlset
+  xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+  xmlns:xhtml="https://www.w3.org/1999/xhtml"
+  xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+  xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+  xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+  xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+>
+  ${pageData.map((elem) => `
   <url>
     <loc>${website}${elem.page}</loc>
     <changefreq>${elem.freq}</changefreq>
@@ -100,9 +96,7 @@ export async function GET() {
 		})}</priority>
     <lastmod>${date}</lastmod>
   </url>
-  `
-		)
-		.join('')}
+  `).join('').trim()}
 		</urlset>`.trim(),
 		{
 			headers: {
