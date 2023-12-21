f6d7d3bed9902f1a2a2dd0be5203bb98b9952f87
Makosai <kristopher.ali.dev@gmail.com>
2023-12-21 06:40:31 -0500

    Create the Layout for the AboutReia article.

---
+++ src/routes/components/articles/_layout.svelte
@@ -0,0 +1,53 @@
+<script>
+	export let title;
+	export let subTitle;
+	export let description;
+	export let tags;
+	export let author;
+	export let date;
+</script>
+
+<article>
+	<h1>{title}</h1>
+	<h2>{subTitle}</h2>
+
+	<slot />
+
+	<p class="date">Last updated: {date}</p>
+</article>
+
+<style>
+	article {
+		@apply mx-10 md:mx-14 text-secondary-400;
+	}
+
+	article h1 {
+		@apply h1 text-center font-bold my-5;
+	}
+
+	article h2 {
+		@apply h3 text-center font-medium mb-10;
+	}
+
+	article .date {
+		@apply text-sm font-bold text-right;
+	}
+
+	article :global(p) {
+		@apply my-5 indent-4;
+	}
+
+	article :global(h3) {
+		@apply h4 mt-10 font-bold max-w-[70%] bg-[rgba(0,0,0,0.1)] pt-4 px-2;
+	}
+
+	article :global(h4) {
+		@apply h6 mb-4 font-medium  max-w-[70%] bg-[rgba(0,0,0,0.1)] pb-4 px-2;
+	}
+
+	article :global(hr) {
+		@apply max-w-[70%] mx-auto;
+
+		border-color: rgba(255, 255, 255, 0.5) !important;
+	}
+</style>
