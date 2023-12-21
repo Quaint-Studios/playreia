74e411de33df438dd860d6097e8a5d337c82ad93
Makosai <kristopher.ali.dev@gmail.com>
2023-12-21 06:39:41 -0500

    Install mdsvex for markdown support.

---
+++ mdsvex.config.js
@@ -0,0 +1,18 @@
+import { defineMDSveXConfig as defineConfig } from 'mdsvex';
+
+const config = defineConfig({
+	extensions: ['.svelte.md', '.md', '.svx'],
+
+  layout: {
+    article: './src/routes/components/articles/_layout.svelte'
+  },
+
+	smartypants: {
+		dashes: 'oldschool'
+	},
+
+	remarkPlugins: [],
+	rehypePlugins: []
+});
+
+export default config;
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,20 @@
+import mdsvexConfig from './mdsvex.config.js';
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte'],
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess()],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexConfig)],
 
 	vitePlugin: {
 		inspector: true
 	},
+
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
