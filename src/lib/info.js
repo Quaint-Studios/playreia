6df004024f8755885416f51cd5d725dfea8791df
Makosai <kristopher.ali.dev@gmail.com>
2023-12-22 09:06:30 -0500

    Create socials links.

--- src/lib/info.js
+++ src/lib/info.js
@@ -1,2 +1,38 @@
 export const name = 'Reia';
 export const website = 'https://www.playreia.com';
+
+
+/**
+ * @typedef {{
+*   github: string;
+*   facebook: string;
+*   instagram: string;
+*   twitter: string;
+*   tiktok: string;
+*   youtube: string;
+*  }} Socials
+ */
+
+/**
+ * @type {Socials}
+ */
+export const socials = {
+  github: 'https://github.com/Quaint-Studios/Reia',
+  facebook: 'https://facebook.com/playreia',
+  instagram: 'https://instagram.com/playreia',
+  twitter: 'https://twitter.com/playreia',
+  tiktok: 'https://tiktok.com/@playreia',
+  youtube: 'https://youtube.com/@playreia'
+}
+
+/**
+ * @type {Socials}
+ */
+export const socialsLocal = {
+  github: 'https://github.playreia.com',
+  facebook: 'https://facebook.playreia.com',
+  instagram: 'https://instagram.playreia.com',
+  twitter: 'https://twitter.playreia.com',
+  tiktok: 'https://tiktok.playreia.com',
+  youtube: 'https://youtube.playreia.com'
+}
