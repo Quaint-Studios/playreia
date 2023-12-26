b2bacac2024a719d31360224dcbb82ccf609e3ae
Makosai <kristopher.ali.dev@gmail.com>
2023-12-25 22:05:26 -0500

    Add a way to hide the navigation from the footer.

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -6,6 +6,8 @@
 	import Twitter from 'svelte-material-icons/Twitter.svelte';
 	import TikTok from 'svelte-material-icons/Circle.svelte';
 	import YouTube from 'svelte-material-icons/Youtube.svelte';
+
+	export let hide_nav = false;
 </script>
 
 <footer>
@@ -17,29 +19,35 @@
 		<a href={socials.tiktok}><TikTok /></a>
 		<a href={socials.youtube}><YouTube /></a>
 	</div>
-	<div class="info">
-		<div class="game-info">
-			<div class="font-bold">Game Info</div>
-			<ul>
-				<li><a href="/">Home</a></li>
-				<li><a href="/blog">Blog</a></li>
-				<li><a href="/spirits">Spirits</a></li>
-				<li><a href="/explore">Explore</a></li>
-			</ul>
+	{#if !hide_nav}
+		<div class="info">
+			<div class="game-info">
+				<div class="font-bold">Game Info</div>
+				<ul>
+					<li><a href="/">Home</a></li>
+					<li><a href="/blog">Blog</a></li>
+					<li><a href="/spirits">Spirits</a></li>
+					<li><a href="/explore">Explore</a></li>
+				</ul>
+			</div>
+			<div class="other-info">
+				<div class="font-bold">Other Info</div>
+				<ul>
+					<li><a href="/press-kit">Press-Kit</a></li>
+					<li><a href="/about">About Us</a></li>
+					<li><a href="/contribute">Contribute</a></li>
+				</ul>
+			</div>
 		</div>
-		<div class="other-info">
-			<div class="font-bold">Other Info</div>
-			<ul>
-				<li><a href="/press-kit">Press-Kit</a></li>
-				<li><a href="/about">About Us</a></li>
-				<li><a href="/contribute">Contribute</a></li>
-			</ul>
+	{/if}
+	<div class="legal-info">
+		<div>Copyright © 2023 Quaint Studios</div>
+		<div>
+			<a href="/terms-and-conditions">Terms & Conditions</a> |
+			<a href="/privacy-policy">Privacy Policy</a>
+			| <a href="/cookies">Cookies</a>
 		</div>
 	</div>
-  <div class="legal-info">
-    <div>Copyright © 2023 Quaint Studios</div>
-    <div><a href="/terms-and-conditions">Terms & Conditions</a> | <a href="/privacy-policy">Privacy Policy</a> | <a href="/cookies">Cookies</a></div>
-  </div>
 </footer>
 
 <style>
@@ -55,7 +63,7 @@
 		@apply w-full flex gap-16 justify-center items-start bg-primary-600 p-16 pt-10;
 	}
 
-  footer .legal-info {
-    @apply w-full flex justify-between items-center bg-primary-600 py-5 px-16 border-t-[1px] border-[rgba(0,0,0,0.2)];
-  }
+	footer .legal-info {
+		@apply w-full flex justify-between items-center bg-primary-600 py-5 px-16 border-t-[1px] border-[rgba(0,0,0,0.2)];
+	}
 </style>
