34b116cfadae03cd9c99e29f0d25266689e2320f
Makosai <kristopher.ali.dev@gmail.com>
2024-07-09 22:38:02 -0400

    Remove the nested button in an anchor.

--- src/components/navigation/NavBrand.svelte
+++ src/components/navigation/NavBrand.svelte
@@ -5,15 +5,15 @@
 	$: hideNav = () => ($navShown = false);
 </script>
 
-<a href="/" id="brand" class="un-a">
-	<button id="brand-logo" on:click={hideNav}>
+<a href="/" id="brand" class="un-a" on:click={hideNav}>
+	<div id="brand-logo">
 		<enhanced:img
 			src="$assets/images/logos/reia_logo_white_transparent.png?enhanced&w=184;168"
 			alt="Reia Logo White Transparent"
 			title="Reia"
 			sizes="(min-width: 768px) 84px, 92px"
 		/>
-	</button>
+	</div>
 	<div class="hidden lg:block h-11 mx-4 border-r-2 border-solid" />
 	<div id="brand-name">{sitename}</div>
 </a>
