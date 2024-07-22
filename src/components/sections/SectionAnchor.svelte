12c7228fc1e37b4cb728f2fe7ade36158df1b334
Makosai <kristopher.ali.dev@gmail.com>
2024-07-22 03:00:52 -0400

    Improve the hero section.

---
+++ src/components/sections/SectionAnchor.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let href: string;
+</script>
+
+<a {href} class="btn un-a section-button-filled">
+	<slot />
+</a>
+
+<style>
+  .section-button-filled {
+    @apply inline-block px-6 py-3 text-white bg-primary-500 rounded-xl shadow-md font-medium;
+  }
+</style>
--- src/components/sections/hero/HeroSection.svelte
+++ src/components/sections/hero/HeroSection.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
-	import Divider from '../Divider.svelte';
 	import Lazy from 'svelte-lazy';
+
+	import Divider from '../Divider.svelte';
+	import SectionAnchor from '../SectionAnchor.svelte';
+	import SectionButton from '../SectionButton.svelte';
 </script>
 
 <section class="section hero">
@@ -13,13 +16,8 @@
 		</h3>
 
 		<div class="cta">
-			<a href="/play" class="btn primary">Play Now</a>
-			<Divider text="or" />
-			<form>
-				<label for="email">Email</label>
-				<input type="email" id="email" name="email" placeholder="Enter your email" />
-				<button type="submit" class="btn secondary variant-filled">Notify Me</button>
-			</form>
+			<SectionAnchor href="/play">Play Now</SectionAnchor>
+			<SectionButton>Join the Newsletter</SectionButton>
 		</div>
 	</div>
 	<div class="hero-image">
@@ -32,17 +30,61 @@
 			/>
 		</Lazy>
 	</div>
+	<div class="hero-gradient" />
 </section>
 
 <style lang="postcss">
 	.hero {
-		@apply relative;
-		@apply pt-[105px] pb-40;
+		@apply relative w-full;
+		@apply pt-[105px];
 	}
 
 	.hero-image {
 		@apply absolute w-full top-0 left-0;
-		@apply object-cover h-[calc(100%+10rem)];
+		@apply object-cover h-[calc(100%+5rem)];
 		z-index: -1;
+	}
+
+	.hero-content {
+		@apply pt-24 px-10 pb-10;
+		@apply max-w-[370rpx] sm:max-w-[390px] md:max-w-[614px];
+	}
+	.hero-content h2 {
+		@apply block text-4xl sm:text-4xl md:text-5xl lg:text-5xl font-bold;
+	}
+	.hero-content h3 {
+		@apply block font-medium pt-8;
+	}
+	.hero-content strong {
+		@apply font-bold text-secondary-500;
+	}
+
+	.cta {
+		@apply pt-8 pr-8;
+		@apply flex gap-4 items-center justify-start;
+	}
+
+	.hero-gradient {
+		@apply w-full absolute mb-[-10rem] top-0 left-0;
+		z-index: -1;
+		height: calc(100% + 10rem);
+		background-image: linear-gradient(
+			to bottom right,
+			rgba(0, 0, 0, 0.4),
+			50%,
+			rgba(0, 0, 0, 0) 60%,
+			rgba(0, 0, 0, 0)
+		);
+	}
+
+	@media (min-width: 768px) {
+		.hero-gradient {
+			background-image: linear-gradient(
+				to right,
+				rgba(0, 0, 0, 0.4),
+				rgba(0, 0, 0, 0) 50%,
+				rgba(0, 0, 0, 0)
+			) !important;
+		}
 	}
 </style>
