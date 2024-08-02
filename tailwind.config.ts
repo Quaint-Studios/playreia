7c7494c0d371d0874a364e1bcf473ad048cb6d1d
Makosai <kristopher.ali.dev@gmail.com>
2024-08-02 02:11:49 -0400

    Add the ml screen size to tailwind.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,35 @@
-import { join } from 'path'
-import type { Config } from 'tailwindcss'
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import { skeleton } from '@skeletonlabs/tw-plugin';
-import { reia_theme } from './src/reia_theme'
+import { reia_theme } from './src/reia_theme';
 
 export default {
 	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
 	theme: {
 		extend: {},
+		screens: {
+			xs: '320px',
+			sm: '640px',
+			md: '768px',
+			ml: '860px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1536px'
+		}
 	},
 	plugins: [
 		forms,
 		typography,
 		skeleton({
 			themes: {
-				custom: [
-					reia_theme,
-				],
-			},
-		}),
-	],
+				custom: [reia_theme]
+			}
+		})
+	]
 } satisfies Config;
