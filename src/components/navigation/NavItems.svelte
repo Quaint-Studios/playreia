3b2cfa1e8c6eccff6bb0e6372a10e0d45ddbcc61
Makosai <kristopher.ali.dev@gmail.com>
2024-08-04 20:12:40 -0400

    Separate NavItems data into a TypeScript file.

--- src/components/navigation/NavItems.svelte
+++ src/components/navigation/NavItems.svelte
@@ -1,70 +1,9 @@
 <script lang="ts">
 	import Icon, { loadIcons } from '@iconify/svelte';
+	import navItems from './NavItemsData';
 
 	// Preload Icons
 	loadIcons(['mdi:chevron-down']);
-
-	// Must not have children more than 3 levels deep
-	const navItems = [
-		{ name: 'Home', href: '/home' },
-		{ name: 'Blog', href: '/blog' },
-		{
-			name: 'Learn',
-			href: '/learn',
-			children: [
-				{
-					name: 'Characters',
-					href: '/learn/characters',
-					children: [
-						{ name: 'Spirits', href: '/learn/characters/spirits' },
-						{ name: 'NPCs', href: '/learn/characters/npcs' },
-						{ name: 'Monsters', href: '/learn/characters/monsters' },
-						{ name: 'Bosses', href: '/learn/characters/bosses' }
-					]
-				},
-				{
-					name: 'Items',
-					href: '/learn/items',
-					children: [
-						{ name: 'Weapons', href: '/learn/items/weapons' },
-						{ name: 'Soulstones', href: '/learn/items/soulstones' },
-						{ name: 'Consumables', href: '/learn/items/consumables' },
-						{ name: 'Quest Items', href: '/learn/items/quest-items' },
-						{ name: 'Equipment', href: '/learn/items/equipment' },
-						{ name: 'Materials', href: '/learn/items/materials' }
-					]
-				},
-				{
-					name: 'Locations',
-					href: '/learn/locations',
-					children: [
-						{ name: 'Cities', href: '/learn/locations/cities' },
-						{ name: 'Dungeons', href: '/learn/locations/dungeons' },
-						{ name: 'Infinite City', href: '/learn/locations/infinite-city' }
-					]
-				},
-				{
-					name: 'Lore',
-					href: '/learn/lore',
-					children: [{ name: 'Ethereals', href: '/learn/lore/ethereals' }]
-				},
-				{ name: 'Skills', href: '/learn/skills' },
-				{ name: 'Gameplay', href: '/learn/gameplay' },
-				{ name: 'Guides', href: '/learn/guides' }
-			]
-		},
-		{ name: 'Press-Kit', href: '/press-kit' },
-		{ name: 'About Us', href: '/about-us' },
-		{
-			name: 'Contribute',
-			href: '/contribute',
-			children: [
-				{ name: 'Backers', href: '/contribute/backers' },
-				{ name: 'GitHub', href: 'https://github.com/Quaint-Studios/Reia' }
-			]
-		},
-		{ name: 'Jobs', href: '/jobs' }
-	];
 </script>
 
 <div class="nav-items">
@@ -166,7 +105,7 @@
 		@apply opacity-50;
 	}
 	a:active {
-		@apply text-tertiary-600;
+		@apply !opacity-25 !text-on-primary-token;
 	}
 
 	.nav-children {
