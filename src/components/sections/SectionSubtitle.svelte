8f5f8dceb1adb6392e80c220f75cdd3be3c3ebb1
Makosai <kristopher.ali.dev@gmail.com>
2024-09-12 22:16:55 -0400

    Create the title and subtitle section components.

---
+++ src/components/sections/SectionSubtitle.svelte
@@ -0,0 +1,9 @@
+<p class="section-subtitle">
+	<slot />
+</p>
+
+<style lang="postcss">
+	.section-subtitle {
+		@apply text-center text-sm font-medium opacity-50;
+	}
+</style>
---
+++ src/components/sections/SectionTitle.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+
+	const icons = ['mdi:minus', 'mdi:minus', 'mdi:minus', 'mdi:circle-medium', 'mdi:rhombus'];
+</script>
+
+<div class="section-title">
+	{#each icons as icon}
+		<Icon {icon} />
+	{/each}
+	<h2><slot /></h2>
+	{#each icons.slice().reverse() as icon}
+		<Icon {icon} />
+	{/each}
+</div>
+
+<style lang="postcss">
+	.section-title {
+		@apply flex items-center justify-center;
+	}
+
+	h2 {
+		@apply mx-4 my-0;
+	}
+</style>
