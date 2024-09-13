0838f7abd5a9b86865feec0658e7fe23f9520d3a
Makosai <kristopher.ali.dev@gmail.com>
2024-09-12 22:19:02 -0400

    Create the start of the DownloadSection and use it.

---
+++ src/components/sections/download/DownloadSection.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import SectionTitle from '../SectionTitle.svelte';
+	import SectionSubtitle from '../SectionSubtitle.svelte';
+</script>
+
+<div class="section download">
+	<SectionTitle>Coming Soon</SectionTitle>
+	<SectionSubtitle>Play Reia across multiple platforms</SectionSubtitle>
+	<div class="">Hi</div>
+</div>
+
+<style lang="postcss">
+	.section {
+		@apply !bg-[rgb(var(--color-primary-800))];
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import HeroSection from '$components/sections/hero/HeroSection.svelte';
+	import DownloadSection from '$components/sections/download/DownloadSection.svelte';
 </script>
 
 <HeroSection />
+<DownloadSection />
