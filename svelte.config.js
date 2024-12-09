c05a6d0a68b5d80cafc08bf0df810316f0526bbb
Makosai <kristopher.ali.dev@gmail.com>
2024-12-09 18:29:49 -0500

    Add the Home layout.

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,11 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [vitePreprocess(), mdsvex({
+		layout: {
+			home: './src/lib/layouts/Home.layout.svelte'
+		}
+	})],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
