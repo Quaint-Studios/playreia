a765775f791a376d08d13ae010280100bfce4ef4
Makosai <kristopher.ali.dev@gmail.com>
2024-12-12 20:16:01 -0500

    Preload major fonts.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,10 +7,21 @@
 	import '@fontsource/poppins/500.css';
 	import '@fontsource/poppins/700.css';
 
+	import UrbanistWoff2 from '@fontsource-variable/urbanist/files/urbanist-latin-wght-normal.woff2';
+	import Poppins500Woff2 from '@fontsource/poppins/files/poppins-latin-500-normal.woff2';
+	import Poppins700Woff2 from '@fontsource/poppins/files/poppins-latin-700-normal.woff2';
+
 	import Navbar from '$components/navigation/Navbar.svelte';
 
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	{#each [UrbanistWoff2, Poppins500Woff2, Poppins700Woff2] as font}
+		<link rel="preload" href={font} as="font" type="font/woff2" crossorigin="anonymous" />
+	{/each}
+	
+</svelte:head>
 
 <Navbar />
 
