03f91953393a04f9d7e5ecda4d56969d57dbe3ce
Makosai <kristopher.ali.dev@gmail.com>
2024-12-16 21:57:27 -0500

    Make html and body at minimum the full height. Add a content and error page.

---
+++ src/lib/components/layout/Content.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<div class="pt-36">
+	{@render children()}
+</div>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Content from '$components/layout/Content.svelte';
+	import Meta from '$components/seo/Meta.svelte';
+
+	const emojis: { [index: number]: string } = {
+		// TODO add the rest!
+		420: '🫠',
+		404: '🧐',
+		500: '💥'
+	};
+</script>
+
+<Meta title="Error... there's ether everywhere!" description="Uh oh, an error happened!">
+	{#snippet structured()}
+		<script type="application/ld+json">
+			{
+				"@context": "https://schema.org/",
+				"@type": "WebSite",
+				"name": "Reia",
+				"url": "https://www.playreia.com"
+			}
+		</script>
+	{/snippet}
+</Meta>
+
+<div class="flex h-dvh w-full flex-col items-center justify-center text-white">
+	<div>
+		<h1 class="text-4xl">
+			<span>
+				{emojis[$page.status] ?? emojis[500]}
+			</span>
+			{$page.status}: {$page.error?.message}
+		</h1>
+
+		<h2 class="my-2 text-xl">Whooops! We're leaking ether...</h2>
+		<p class="my-8 text-center">
+			Lets go back to the <a href="/">home page</a>.
+		</p>
+	</div>
+</div>
