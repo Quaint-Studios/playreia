aa8b95c6b3b6ade9a89fbaf8e9969333bde13f75
Makosai <kristopher.ali.dev@gmail.com>
2024-12-18 02:15:54 -0500

    Start implementing BlogItem.

---
+++ src/lib/components/layout/BlogItem.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		/** The blog item title. */
+		title: string;
+		/** The blog item description. */
+		description: string;
+		/** The blog item image. */
+		image: any;
+		/** The blog item date. */
+		date: string;
+		/** The blog item author. */
+		author: string;
+		/** The call to action. */
+		cta?: string;
+	}
+
+	const { title, description, image, date, author, cta = 'Read this post' }: Props = $props();
+</script>
+
+<enhanced:img src={image} alt={title} class="h-64 w-full object-cover" />
