d7184f0feb2581b3cefd962a78564669c9fd09e6
Makosai <kristopher.ali.dev@gmail.com>
2024-12-19 04:14:58 -0500

    Improve the hero generated sizes.

--- src/lib/components/layout/Hero.svelte
+++ src/lib/components/layout/Hero.svelte
@@ -13,10 +13,10 @@
 		<Lazy keep={true} height="500px">
 			<enhanced:img
 				class="image"
-				src="$images/hero/hero_1.jpg?enhanced&w=3840;2048;1920;1280"
+				src="$images/hero/hero_1.jpg?enhanced&w=3840;2048;1920;1280;1024"
 				alt="A forest, flower field, and path"
 				title="Flower Field and Path"
-				sizes="min(1280px, 100vw)"
+				sizes="(min-width: 3840px) 3840px, (min-width: 2048px) 2048px, (min-width: 1536px): 1920px, (min-width: 1280px) 1280px"
 				loading="lazy"
 			/>
 		</Lazy>
