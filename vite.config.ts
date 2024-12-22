8dbc7300494733f98b3ab7ca78394711636a5f57
Makosai <kristopher.ali.dev@gmail.com>
2024-12-22 15:19:47 -0500

    PurgeCSS.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 
 export default defineConfig({
 	plugins: [
 		enhancedImages(),
 		sveltekit(),
+		purgeCss(),
 		paraglide({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide'
