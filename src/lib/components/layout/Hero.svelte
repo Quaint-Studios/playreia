0adc6e7db592bba33c4862005b8a10a39b3549cb
Makosai <kristopher.ali.dev@gmail.com>
2024-12-22 16:47:32 -0500

    Lazy load hero.

--- src/lib/components/layout/Hero.svelte
+++ src/lib/components/layout/Hero.svelte
@@ -17,6 +17,7 @@
 				alt="A forest, flower field, and path"
 				title="Flower Field and Path"
 				sizes="(min-width: 3840px) 3840px, (min-width: 2048px) 2048px, (min-width: 1536px): 1920px, (min-width: 1280px) 1280px"
+				loading="lazy"
 			/>
 		</Lazy>
 	</div>
