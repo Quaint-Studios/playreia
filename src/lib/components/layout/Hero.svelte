f94771eebabf44d8d32f4ff5bb76fdaeaa5bc640
Makosai <kristopher.ali.dev@gmail.com>
2024-12-22 16:59:07 -0500

    Improve Hero component.

--- src/lib/components/layout/Hero.svelte
+++ src/lib/components/layout/Hero.svelte
@@ -17,7 +17,8 @@
 				alt="A forest, flower field, and path"
 				title="Flower Field and Path"
 				sizes="(min-width: 3840px) 3840px, (min-width: 2048px) 2048px, (min-width: 1536px): 1920px, (min-width: 1280px) 1280px"
-				loading="lazy"
+				loading="eager"
+				fetchpriority="high"
 			/>
 		</Lazy>
 	</div>
@@ -53,7 +54,7 @@
 				hoverColor={colors.black}
 				roundness="large"
 				size="large"
-				--color="--white"
+				--color="var(--white)"
 				--bg-color="var(--blueHighlight)"
 				--hover-bg-color="var(--lightBlue)"
 			>
