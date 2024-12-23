8c18351578bbdf83f0dacd66043bf9e222559d8c
Makosai <kristopher.ali.dev@gmail.com>
2024-12-22 22:09:25 -0500

    Update About Us page.

--- src/routes/about-us/+page.svelte
+++ src/routes/about-us/+page.svelte
@@ -1,5 +1,46 @@
 <script lang="ts">
-	import Todo from '$components/layout/Todo.svelte';
+	import Section from '$components/layout/Section.svelte';
+	import Content from '$components/layout/Content.svelte';
+	import Meta from '$components/seo/Meta.svelte';
 </script>
 
-<Todo />
+<!-- <TODO> PAGE -->
+<Meta
+	title="About Us"
+	description="Learn all about Reia, the team, and the development journey. Find out why this project was started, our process, what we've learned, and what we plan to do!"
+>
+	{#snippet structured()}
+		<script type="application/ld+json">
+			{
+				"@context": "https://schema.org/",
+				"@type": "WebSite",
+				"name": "Reia",
+				"url": "https://www.playreia.com/terms-and-conditions"
+			}
+		</script>
+	{/snippet}
+</Meta>
+
+<Content>
+	<Section>
+		<div class="content">
+			<h1>About Us: Reia and the Team</h1>
+			<strong>
+				Read on to figure out why this project was started and the many people who have helped on
+				the side and will be helping as main supporters.
+			</strong>
+			<br /><br />
+			<p>This page is still under construction but will have information soon.</p>
+		</div>
+	</Section>
+</Content>
+
+<style lang="postcss">
+	h1 {
+		@apply w-full text-center;
+	}
+
+	.content {
+		@apply mx-auto max-w-screen-md;
+	}
+</style>
