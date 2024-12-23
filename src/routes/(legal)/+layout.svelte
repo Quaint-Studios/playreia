7583a7660bbad19007ebd812469077ff4d85abba
Makosai <kristopher.ali.dev@gmail.com>
2024-12-22 20:28:59 -0500

    Update legal pages.

---
+++ src/routes/(legal)/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Content from '$components/layout/Content.svelte';
+	import Section from '$components/layout/Section.svelte';
+
+	let { children } = $props();
+</script>
+
+<Content>
+	<Section>
+		<div class="legal">
+			{@render children()}
+		</div>
+	</Section>
+</Content>
--- src/routes/privacy/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import Todo from '$components/layout/Todo.svelte';
-</script>
-
-<Todo />
