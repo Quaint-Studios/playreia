1d560161add94a7399d8cd2bd32814a6851ba1ba
Makosai <kristopher.ali.dev@gmail.com>
2024-12-28 17:56:53 -0500

    Increase the size of the hero to 960px.

--- src/lib/components/layout/Hero.svelte
+++ src/lib/components/layout/Hero.svelte
@@ -86,7 +86,7 @@
 <style lang="postcss">
 	.hero {
 		@apply bg-[--deepPurpleHighlight];
-		@apply h-[45rem] w-full;
+		@apply h-[60rem] w-full;
 		@apply text-[--light];
 		@apply relative;
 	}
@@ -116,7 +116,7 @@
 	}
 
 	.hero .image {
-		@apply absolute top-0 h-[45rem] w-full object-cover object-bottom;
+		@apply absolute top-0 h-[60rem] w-full object-cover object-bottom;
 	}
 
 	.section-gradient {
