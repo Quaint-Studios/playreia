2e4a65f7a48e00b7789f5ad1d2d0855955065714
Makosai <kristopher.ali.dev@gmail.com>
2025-01-13 02:39:42 -0500

    Update NewsletterButton.svelte

--- src/lib/components/footers/NewsletterButton.svelte
+++ src/lib/components/footers/NewsletterButton.svelte
@@ -44,14 +44,19 @@
 	let email: string | undefined = $state(undefined);
 	let password: string | undefined = $state(undefined);
 
-	let { alt = false, id = '' }: { alt?: boolean, id?: string } = $props();
+	let { alt = false, id = '' }: { alt?: boolean; id?: string } = $props();
 </script>
 
 <div class="container" class:alt>
+	{#if status === 'error'}
+		<div class="absolute left-0 right-0 -top-5 text-[#ff4646] text-sm text-center">
+			<p>There was an error. Please try again.</p>
+		</div>
+	{/if}
 	<form autocomplete="off">
 		<input
 			class:error={status === 'error'}
-			class = "email"
+			class="email"
 			aria-label="Newsletter"
 			type="email"
 			id="newsletter-{id}email"
@@ -87,7 +92,6 @@
 </div>
 
 <style lang="postcss">
-
 	.container {
 		@apply relative mt-5 flex h-min w-full max-w-[325px] items-center justify-center;
 	}
@@ -101,10 +105,10 @@
 		@apply text-[--borderSilver];
 	}
 	.container input.email.error {
-		@apply !border-[1px] !border-solid !border-[#ff4646] border-opacity-70;
+		@apply !border-solid !border-[#ff4646] border-opacity-70;
 	}
 	.container input.email:focus {
-		@apply border-[1px] border-solid border-[--borderSilver] !bg-[--midnight5];
+		@apply border-solid border-transparent !bg-[--midnight5];
 		box-shadow: 0 0 0 1px var(--midnight7);
 	}
 
@@ -124,6 +128,6 @@
 		@apply scale-[0.98];
 	}
 	.container .email {
-		@apply w-full rounded-xl !border-none !outline-none;
+		@apply w-full rounded-xl !border-[1px] border-transparent !outline-none;
 	}
 </style>
