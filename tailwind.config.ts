48b6de06d71690e55bf31f922faf29ca83794533
Makosai <kristopher.ali.dev@gmail.com>
2025-01-18 15:16:04 -0500

    Update the colors for Tailwind to reduce HTML.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,75 @@ export default {
 			},
 			borderRadius: {
 				'lg2': '0.625rem'
+			},
+			colors: {
+				'r': {
+					primary: '#7666c1',
+					secondary: '#ffffff',
+					tertiary: '#f9f3e5',
+					success: '#77dd77',
+					danger: '#ff6961',
+					warning: '#ffd700',
+					info: '#add8e6',
+					link: '#b0c4de',
+
+					border: {
+						default: '#f4f4f4',
+						"0.5": '#f4f4f480',
+						"0.25": '#f4f4f440',
+						"blue-0.5" : '#86c7f980',
+						silver: '#d2d9e7'
+					},
+
+					silver: {
+						dark: '#2b364c',
+						light: '#94a2b7'
+					},
+
+					"gray": {
+						text: '#808080',
+						button: '#d3d3d3'
+					},
+
+					light: '#f0f8ff',
+					dark: '#323232',
+					white: '#ffffff',
+					black: '#000000',
+					"light-dark": '#424242',
+
+					gold: '#fed18e',
+					"gold-0.5": '#fed18ebb',
+					"gold-2": '#fdbd5d',
+
+					red: '#fc3c35',
+					"crimson-red": '#ff4646bf',
+					yellow: '#f9fc35',
+
+					blue: {
+						light: '#47aaf5',
+						default: '#239af4',
+						highlight: '#0b78cb',
+						deep: '#0d47a1'
+					},
+
+					midnight: {
+						blue: '#081247',
+						"icon-bg": '#394690',
+						100: '#101537',
+						200: '#151b47',
+						300: '#172569',
+						400: '#1c2c7d',
+						500: '#2b5073',
+						600: '#39468e',
+						700: '#4757b3'
+					},
+
+					purple: {
+						deep: '#220d54',
+						highlight: '#3a168d',
+						"highlight-0.5": '#3a168d80'
+					}
+				}
 			}
 		}
 	},
