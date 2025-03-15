2a2e496456c201d4f6b758661e494f0226ada240
Makosai <kristopher.ali.dev@gmail.com>
2025-03-15 13:17:49 -0400

    Migrate Skeleton.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,8 @@ import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
-import { skeleton, contentPath } from '@skeletonlabs/skeleton/plugin';
-import * as themes from '@skeletonlabs/skeleton/themes';
-
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}', contentPath(import.meta.url, 'svelte')],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
 		extend: {
@@ -103,8 +100,5 @@ export default {
 		typography,
 		forms,
 		containerQueries,
-		skeleton({
-			themes: [themes.rose]
-		})
 	]
 } satisfies Config;
