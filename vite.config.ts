39b15dd9cd437a8c17c4dba441f2cbe6b15e7c70
Makosai <kristopher.ali.dev@gmail.com>
2025-04-01 13:57:35 -0400

    Go back to using COI.

--- vite.config.ts
+++ vite.config.ts
@@ -13,14 +13,11 @@ const viteServerConfig = () => ({
 	configureServer: (server: ViteDevServer) => {
 		server.middlewares.use((req, res, next) => {
 			switch (req.url) {
-				case '/game':
 				case '/play':
-				case '/index.html':
-				// case '/index.js':
-				// 	res.setHeader('Access-Control-Allow-Origin', '*');
-				// 	res.setHeader('Access-Control-Allow-Methods', 'GET');
-				// 	res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-				// 	res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+					res.setHeader('Access-Control-Allow-Origin', 'https://site.cdn.playreia.com');
+					res.setHeader('Access-Control-Allow-Methods', 'GET HEAD OPTIONS');
+					res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+					res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
 					break;
 			}
 			next();
