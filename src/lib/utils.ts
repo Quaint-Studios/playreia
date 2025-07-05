c9405bb24d3a8af9ebc6cdaab11030dc1089b6ae
Makosai <kristopher.ali.dev@gmail.com>
2025-07-04 23:57:33 -0400

    Add debounce to the list of utils.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,17 @@
 export function validateEmail(email: string): boolean {
 	const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 	return re.test(email);
-  }
+}
+
+export function debounce<T extends unknown[]>(
+	fn: (...args: T) => void,
+	wait = 100
+): (...args: T) => void {
+	let timeout: ReturnType<typeof setTimeout>;
+	return (...args: T) => {
+		clearTimeout(timeout);
+		timeout = setTimeout(() => {
+			fn(...args);
+		}, wait);
+	};
+}
