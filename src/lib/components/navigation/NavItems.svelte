b86415d51ef4b7e50fc90648e511ceb25231f843
Makosai <kristopher.ali.dev@gmail.com>
2025-07-04 23:58:01 -0400

    Add an animated background to nav items.

--- src/lib/components/navigation/NavItems.svelte
+++ src/lib/components/navigation/NavItems.svelte
@@ -8,21 +8,95 @@
 	}
 
 	let { isOpen, toggle }: Props = $props();
+	let hoverIndex = $state(-1);
+	function onHover(index: number) {
+		console.log('hovered');
+		hoverIndex = index;
+		highlightLink();
+	}
+
+	function onLeave() {
+		hoverIndex = -1;
+		cleanHighlighter();
+	}
+
+	function onBlur() {
+		// Check if current focus is a child of the nav items
+		const navItems = document.querySelector('.nav-items');
+
+		if (!navItems) {
+			console.error("Couldn't find the navigation items container.");
+			return;
+		}
+
+		setTimeout(() => {
+			if (document.activeElement && navItems.contains(document.activeElement)) {
+				// If it is, do not clean the highlighter
+				console.log('active element is within nav items, not cleaning highlighter');
+				return;
+			}
+			// If not, clean the highlighter
+			console.log('active element is outside nav items, cleaning highlighter');
+
+			hoverIndex = -1;
+			cleanHighlighter();
+		}, 0);
+	}
+
+	function highlightLink() {
+		if (hoverIndex < 0 || hoverIndex >= listData.length) {
+			cleanHighlighter();
+			return;
+		}
+		// Set the highlighter position based on the hovered link
+		const id = `nav-link-${listData[hoverIndex]?.name}`;
+		const linkElement = document.getElementById(id);
+		if (linkElement) {
+			const highlighter = document.getElementById('nav-link-highlighter');
+			if (highlighter) {
+				highlighter.style.display = 'block';
+				highlighter.style.width = `${linkElement.offsetWidth + 24}px`;
+				highlighter.style.left = `${linkElement.offsetLeft - 6}px`;
+				setTimeout(() => {
+					highlighter.style.transition = '';
+				}, 0);
+			}
+		}
+	}
+
+	function cleanHighlighter() {
+		const highlighter = document.getElementById('nav-link-highlighter');
+		if (highlighter) {
+			highlighter.style.display = 'none';
+			highlighter.style.width = '0';
+			highlighter.style.left = '0';
+			highlighter.style.transition = 'none'; // Disable transition for immediate effect
+		}
+	}
 </script>
 
-<div class="nav-items">
-	<div class="nav-items-outer" class:open={isOpen} role="menubar">
+<div class="nav-items" onmouseleave={onLeave} role="menubar" tabindex="0">
+	<div class="nav-items-outer" class:open={isOpen}>
 		<div class="nav-items-inner">
-			{#each listData as { name, href, children }}
-				<div class="nav-link">
-					<a aria-label="{name} Page" role="menuitem" {href} onclick={toggle}>
+			<div id="nav-link-highlighter" style={"transition: none"}></div>
+			{#each listData as { name, href, children }, index}
+				<div class="nav-link" id="nav-link-{name}">
+					<a
+						aria-label="{name} Page"
+						role="menuitem"
+						{href}
+						onclick={toggle}
+						onmouseover={() => onHover(index)}
+						onfocus={() => onHover(index)}
+						onblur={onBlur}
+					>
 						<span class="w-full drop-shadow-lg">{name}</span>
 						{#if children}
 							<DownSmalllFill class="shrink-0 text-2xl" />
 						{/if}
 					</a>
 					{#if children}
-						<div class="nav-link-child glass">
+						<div class="nav-link-child">
 							{#each children as { name, href }}
 								<div class="nav-link">
 									<a aria-label="{name} Page" role="menuitem" {href}>
@@ -40,6 +114,11 @@
 
 <style lang="postcss">
 	@reference '$appcss';
+
+	#nav-link-highlighter {
+		@apply bg-white/20 rounded-full -ml-2 hidden h-8 px-2 transition-[width,left] duration-300;
+		@apply absolute top-1/2 left-0 -translate-y-1/2 pointer-events-none;
+	}
 
 	.nav-items {
 		@apply grow font-bold;
@@ -77,6 +156,11 @@
 		}
 
 		min-height: 0;
+		@apply relative;
+	}
+
+	.nav-link {
+		@apply flex items-stretch h-[40px];
 	}
 
 	.nav-link a {
