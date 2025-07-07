e8aacd0e78cd83ae15f9372706f8f1e9f51be4c5
Makosai <kristopher.ali.dev@gmail.com>
2025-07-06 20:53:37 -0400

    Remove icon from ListData.

--- src/lib/components/navigation/ListData.ts
+++ src/lib/components/navigation/ListData.ts
@@ -1,7 +1,6 @@
 export interface NavItem {
 	name: string;
 	href: string;
-	icon?: string;
 	description?: string;
 	children?: NavItem[];
 }
@@ -18,13 +17,11 @@ export default [
 			{
 				name: 'Explore Reia',
 				href: '/learn',
-				icon: 'solar:book-bookmark-minimalistic-bold',
 				description: 'Information on everything there is.'
 			},
 			{
 				name: 'Characters',
 				href: '/learn/characters',
-				icon: 'fluent:people-16-filled',
 				description: 'Spirits, NPCs, Bosses, and more.',
 				children: [
 					{ name: 'Spirits', href: '/learn/characters/spirits' },
@@ -36,7 +33,6 @@ export default [
 			{
 				name: 'Items',
 				href: '/learn/items',
-				icon: 'hugeicons:potion',
 				description: 'Stuff like weapons and soulstones!',
 				children: [
 					{ name: 'Weapons', href: '/learn/items/weapons' },
@@ -50,7 +46,6 @@ export default [
 			{
 				name: 'Locations',
 				href: '/learn/locations',
-				icon: 'icons8:adventures',
 				description: 'Where you can go in Reia.',
 				children: [
 					{ name: 'Cities', href: '/learn/locations/cities' },
@@ -61,24 +56,38 @@ export default [
 			{
 				name: 'Lore',
 				href: '/learn/lore',
-				icon: 'ion:book',
 				description: 'What was and is.',
 				children: [{ name: 'Ethereals', href: '/learn/lore/ethereals' }]
 			},
-			{ name: 'Skills', href: '/learn/skills', icon: 'game-icons:comet-spark', description: 'Big booms and damage.' },
-			{ name: 'Gameplay', href: '/learn/gameplay', icon: 'tabler:cube-spark', description: 'What makes Reia fun.' },
-			{ name: 'Guides', href: '/learn/guides', icon: 'ep:guide', description: 'Be better. Play better. Learn here!' }
+			{
+				name: 'Skills',
+				href: '/learn/skills',
+				description: 'Big booms and damage.'
+			},
+			{
+				name: 'Gameplay',
+				href: '/learn/gameplay',
+				description: 'What makes Reia fun.'
+			},
+			{
+				name: 'Guides',
+				href: '/learn/guides',
+				description: 'Be better. Play better. Learn here!'
+			}
 		]
 	},
 	{
 		name: 'Press-Kit',
 		href: '/press-kit',
 		children: [
-			{ name: 'Browse Press-Kit', href: '/press-kit', icon: 'typcn:camera', description: 'Write a story about us!' },
+			{
+				name: 'Browse Press-Kit',
+				href: '/press-kit',
+				description: 'Write a story about us!'
+			},
 			{
 				name: 'Game Design Document',
 				href: '/game-design-document',
-				icon: 'fluent:design-ideas-16-filled',
 				description: 'The design document for Reia.'
 			}
 		]
@@ -87,10 +96,22 @@ export default [
 		name: 'Contribute',
 		href: '/contribute',
 		children: [
-			{ name: 'How to Help', href: '/contribute', icon: 'mdi:help-outline', description: 'Learn how to help us.' },
-			{ name: 'Backers', href: '/contribute/backers', icon: 'icon-park-outline:funds', description: 'Our amazing supporters.' },
-			{ name: 'GitHub', href: 'https://github.com/Quaint-Studios/Reia', icon: 'fa-brands:github-alt', description: 'The secret sauce.' },
+			{
+				name: 'How to Help',
+				href: '/contribute',
+				description: 'Learn how to help us.'
+			},
+			{
+				name: 'Backers',
+				href: '/contribute/backers',
+				description: 'Our amazing supporters.'
+			},
+			{
+				name: 'GitHub',
+				href: 'https://github.com/Quaint-Studios/Reia',
+				description: 'The secret sauce.'
+			},
 			{ name: 'Jobs', href: '/jobs', description: 'Come work with us!' }
 		]
-	},
+	}
 ] as NavItem[];
