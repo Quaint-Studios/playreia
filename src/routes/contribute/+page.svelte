de2693f6ccad8105e5d3a7a3821711c8e9932183
Makosai <kristopher.ali.dev@gmail.com>
2025-07-27 23:43:18 -0400

    Update the seo for contribute and backers pages.

--- src/routes/contribute/+page.svelte
+++ src/routes/contribute/+page.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
 	import Content from '$components/layout/Content.svelte';
 	import Section from '$components/layout/Section.svelte';
-	import Meta from '$components/seo/Meta.svelte';
+	import Seo from '$components/seo/Seo.svelte';
 	import alinks from '$constants/alinks';
+
+	let title = 'Become an Open-Source Contributor';
+	let description =
+		"Contribute to Reia's development. Whether you're a programmer, artist, designer, or writer, there's a place for you in the Reia community.";
+	// prettier-ignore
+	let keywords = ['reia', 'contribute', 'contribute to reia', 'open-source', 'open-source contributor', 'open-source contribution', 'open-source project', 'open-source development', 'open-source community', 'open-source software', 'open-source game', 'open-source game development', 'open-source game design', 'open-source game art', 'open-source game writing', 'open-source game programming', 'open-source game developer', 'open-source game designer', 'open-source game artist', 'open-source game writer', 'open-source game programmer', 'open-source game development community', 'open-source game design community', 'open-source game art community', 'open-source game writing community', 'open-source game programming community', 'open-source game developer community', 'open-source game designer community', 'open-source game artist community', 'open-source game writer community', 'open-source game programmer community']
+	let { data } = $props();
 </script>
 
-<Meta
-	titlePrefix=""
-	title="Reia - Become an Open-Source Contributor"
-	titleSuffix=""
-	keywords={'reia, contribute, contribute to reia, open-source, open-source contributor, open-source contribution, open-source project, open-source development, open-source community, open-source software, open-source game, open-source game development, open-source game design, open-source game art, open-source game writing, open-source game programming, open-source game developer, open-source game designer, open-source game artist, open-source game writer, open-source game programmer, open-source game development community, open-source game design community, open-source game art community, open-source game writing community, open-source game programming community, open-source game developer community, open-source game designer community, open-source game artist community, open-source game writer community, open-source game programmer community'}
-	description="Contribute to Reia's development. Whether you're a programmer, artist, designer, or writer, there's a place for you in the Reia community."
->
-	{#snippet structured()}
-		<script type="application/ld+json">
+<Seo {title} {description} reverseDecoration={true} {keywords} locale={data.locale} url={data.url}>
+	{#snippet structured_data()}
+		{@html `<script type="application/ld+json">
 			{
 				"@context": "https://schema.org/",
 				"@type": "WebSite",
@@ -41,13 +42,15 @@
 					}
 				]
 			}
-		</script>
+		</script>`}
 	{/snippet}
-</Meta>
+</Seo>
 
 <Content>
 	<Section>
-		<h1>Contribute to <strong>Reia</strong></h1>
+		<h1>
+			Contribute to <strong>Reia</strong>
+		</h1>
 		<div class="readable">
 			<blockquote>
 				<p>As an open-source project, there are many ways to contribute to Reia's developemnt.</p>
@@ -60,20 +63,28 @@
 
 			<h2>GitHub Repository</h2>
 			<p>
-				Reia's source code is hosted on <a href={alinks.github} target="_blank" rel="noopener"
-					>GitHub</a
-				>. You can contribute by submitting bug reports, feature requests, or pull requests. This is
-				a good place for <strong>programmers</strong>
-				and <strong>writers</strong> to contribute to the project.
+				Reia's source code is hosted on <a href={alinks.github} target="_blank" rel="noopener">
+					GitHub
+				</a>
+				. You can contribute by submitting bug reports, feature requests, or pull requests. This is a
+				good place for
+				<strong>programmers</strong>
+				and
+				<strong>writers</strong>
+				to contribute to the project.
 			</p>
 
 			<h2>Discord Server</h2>
 			<p>
-				We've finally opened up <a href={alinks.discord} target="_blank" rel="noopener"
-					>Reia's Discord server</a
-				>. Join to discuss the project with other contributors. This is a good place for everyone to
-				collaborate on the project. Especially for <strong>artists</strong> and
-				<strong>designers</strong>.
+				We've finally opened up <a href={alinks.discord} target="_blank" rel="noopener">
+					Reia's Discord server
+				</a>
+				. Join to discuss the project with other contributors. This is a good place for everyone to collaborate
+				on the project. Especially for
+				<strong>artists</strong>
+				and
+				<strong>designers</strong>
+				.
 			</p>
 
 			<h2>Open Collective</h2>
@@ -81,9 +92,14 @@
 				You can support the project financially by donating to the <a
 					href={alinks.open_collective}
 					target="_blank"
-					rel="noopener">Open Collective</a
-				>. The money goes towards hosting, domain registration, and paying open-source and hired
-				contributors. You'll also be listed as a <a href="/contribute/backers">backer</a>!
+					rel="noopener"
+				>
+					Open Collective
+				</a>
+				. The money goes towards hosting, domain registration, and paying open-source and hired contributors.
+				You'll also be listed as a
+				<a href="/contribute/backers">backer</a>
+				!
 			</p>
 		</div>
 	</Section>
--- src/routes/contribute/backers/+page.svelte
+++ src/routes/contribute/backers/+page.svelte
@@ -1,40 +1,46 @@
 <script lang="ts">
 	import Content from '$components/layout/Content.svelte';
 	import Section from '$components/layout/Section.svelte';
-	import Meta from '$components/seo/Meta.svelte';
+	import Seo from '$components/seo/Seo.svelte';
 	import alinks from '$constants/alinks';
+
+	let title = 'Our Amazing Backers and Supporters';
+	let description =
+		"This is a list of all the amazing backers and supporters of the Reia project. This gives recognization to those who have supported the project's game development.";
+	// prettier-ignore
+	const keywords = ['reia', 'contribute', 'contribute to reia', 'open-source', 'open-source contributor', 'open-source contribution', 'open-source project', 'open-source development', 'open-source community', 'open-source software', 'open-source game', 'open-source game development', 'open-source game design', 'open-source game art', 'open-source game writing', 'open-source game programming', 'open-source game developer', 'open-source game designer', 'open-source game artist', 'open-source game writer', 'open-source game programmer', 'open-source game development community', 'open-source game design community', 'open-source game art community', 'open-source game writing community', 'open-source game programming community', 'open-source game developer community', 'open-source game designer community', 'open-source game artist community', 'open-source game writer community', 'open collective', 'open collective backers', 'open collective supporters', 'open collective contributors', 'open collective financial backers', 'open collective financial supporters', 'open collective financial contributors', 'opencollective backers', 'opencollective supporters', 'opencollective contributors', 'opencollective financial backers', 'opencollective financial supporters', 'opencollective financial contributors']
+	let { data } = $props();
 </script>
 
-<Meta
-	titlePrefix=""
-	title="Reia - Our Amazing Backers and Supporters"
-	titleSuffix=""
-	keywords={'reia, contribute, contribute to reia, open-source, open-source contributor, open-source contribution, open-source project, open-source development, open-source community, open-source software, open-source game, open-source game development, open-source game design, open-source game art, open-source game writing, open-source game programming, open-source game developer, open-source game designer, open-source game artist, open-source game writer, open-source game programmer, open-source game development community, open-source game design community, open-source game art community, open-source game writing community, open-source game programming community, open-source game developer community, open-source game designer community, open-source game artist community, open-source game writer community, open collective, open collective backers, open collective supporters, open collective contributors, open collective financial backers, open collective financial supporters, open collective financial contributors, opencollective backers, opencollective supporters, opencollective contributors, opencollective financial backers, opencollective financial supporters, opencollective financial contributors'}
-	description="This is a list of all the amazing backers and supporters of the Reia project. This gives recognization to those who have supported the project's game development."
->
-	{#snippet structured()}
-		<script type="application/ld+json">
+<Seo {title} {description} reverseDecoration={true} {keywords} locale={data.locale} url={data.url}>
+	{#snippet structured_data()}
+		{@html `<script type="application/ld+json">
 			{
 				"@context": "https://schema.org/",
 				"@type": "WebSite",
 				"name": "Reia Supporters and Financial Backers",
 				"url": "https://www.playreia.com/contribute/backers"
 			}
-		</script>
+		</script>`}
 	{/snippet}
-</Meta>
+</Seo>
 
 <Content>
 	<Section>
-		<h1>Reia <strong>Supporters</strong></h1>
+		<h1>
+			Reia <strong>Supporters</strong>
+		</h1>
 		<div class="readable">
 			<blockquote>
 				<p>
 					Below is a list of financial backers that have supported the project through either <a
 						href={alinks.open_collective}
 						target="_blank"
-						rel="noopener">Open Collective</a
-					> or other means we've recognized.
+						rel="noopener"
+					>
+						Open Collective
+					</a>
+					or other means we've recognized.
 				</p>
 			</blockquote>
 
@@ -44,8 +50,11 @@
 				Recurring backers are those who have donated on <a
 					href={alinks.open_collective}
 					target="_blank"
-					rel="noopener">Open Collective</a
-				> to the project either monthly or yearly basis. They are listed here as a thank you for their
+					rel="noopener"
+				>
+					Open Collective
+				</a>
+				to the project either monthly or yearly basis. They are listed here as a thank you for their
 				continued support.
 			</p>
 
@@ -61,8 +70,11 @@
 				here, please donate to the project through our <a
 					href={alinks.open_collective}
 					target="_blank"
-					rel="noopener">Open Collective</a
-				>.
+					rel="noopener"
+				>
+					Open Collective
+				</a>
+				.
 			</p>
 
 			<ul>
@@ -84,7 +96,7 @@
 		@apply text-r-gold;
 	}
 	blockquote {
-		@apply mb-6 mt-4;
+		@apply mt-4 mb-6;
 	}
 	ul {
 		@apply mt-4;
