2b5f891d59e1a85a8e8f2b4ea762eedc884b465a
Makosai <kristopher.ali.dev@gmail.com>
2025-10-01 22:04:53 -0400

    Update the game design document.

--- src/routes/game-design-document/+page.svelte
+++ src/routes/game-design-document/+page.svelte
@@ -1,5 +1,421 @@
 <script lang="ts">
-	import Todo from '$components/layout/Todo.svelte';
 </script>
 
-<Todo />
+<article>
+	<h1>Game Design Document for Reia</h1>
+	<span class="text-r-border-silver/75 mb-8 block text-sm">
+		Last updated: <time datetime="2025-10-01">October 1, 2025</time>
+	</span>
+
+	<h2>Overview</h2>
+	<blockquote>
+		This section outlines the purpose of the document, the target audience, and the concept
+		statement of the game Reia.
+	</blockquote>
+
+	<ol>
+		<li>
+			<b>Name of Project:</b>
+			Reia
+		</li>
+		<li>
+			<b>Purpose of the Document:</b>
+			This document outlines the design of Reia, an open-source action adventure RPG. It details the
+			game's concept, mechanics, world, story, technical aspects, production plan, and more. This document
+			serves as a guide for development and a reference point for anyone interested in the game's design.
+		</li>
+		<li>
+			<b>Concept Statement:</b>
+			Reia is an open-source action adventure RPG where players explore a vast online world, complete
+			quests, battle using spirits and power-enhancing soulstones, create their own player-owned islands,
+			explore a massive infinite city, and more.
+		</li>
+		<li>
+			<b>Target Audience:</b>
+			People who enjoy action RPGs with online multiplayer elements, open world exploration, and deep
+			character customization. The game is designed to be accessible to both casual and hardcore gamers,
+			with a focus on community involvement and creativity.
+		</li>
+	</ol>
+
+	<h2>Game Concept Design</h2>
+	<blockquote>
+		This section details the genre, target audience, unique selling points, overall vision, and core
+		gameplay loop of Reia.
+	</blockquote>
+
+	<h3>Target & Genre</h3>
+	<ol>
+		<li>Genre: Action Adventure RPG (MMORPG)</li>
+		<li>
+			Target Audience: PC gamers who enjoy action RPGs with online multiplayer elements, open world
+			exploration, and deep character customization. Mobile and console players will also be a
+			secondary target audience. And cozy gamers who enjoy crafting, gathering, and building their
+			own spaces will be a tertiary target audience.
+		</li>
+		<li>
+			Unique Selling Points:
+			<ul>
+				<li>Open-source development model allowing for community contribution.</li>
+				<li>Deep character customization with spirits, soulstones, and skill trees.</li>
+				<li>Player-owned islands and a massive, ever-expanding infinite city.</li>
+				<li>Crafting and gathering to create potions, food, gear, spellstones, and more.</li>
+			</ul>
+		</li>
+		<li>
+			Overall Vision and Goals:
+			<ul>
+				<li>
+					Create a vibrant and engaging online world that fosters community and player creativity.
+				</li>
+				<li>
+					Offer deep and rewarding character progression that keeps players engaged for long play
+					sessions.
+				</li>
+				<li>
+					Foster a collaborative development environment where the community can contribute to the
+					game's growth.
+				</li>
+			</ul>
+		</li>
+	</ol>
+
+	<h3>Gameplay</h3>
+	<ol>
+		<li>
+			Core Gameplay Loop: Explore the world, complete quests, battle enemies, collect resources,
+			craft items, progress your character, and socialize with other players.
+		</li>
+		<li>
+			Objectives: Complete quests, defeat challenging enemies, explore dungeons, and uncover the
+			lore of the Reia's world.
+		</li>
+		<li>
+			Progression: Gain experience by playing the game naturally to level up your character / items,
+			unlock new skills and abilities with mastery over time included, and acquire powerful
+			equipment.
+		</li>
+	</ol>
+
+	<h3>Story (details to be added later)</h3>
+	<ol>
+		<li>Main Story</li>
+		<li>Quests</li>
+		<li>Mini-Quests</li>
+		<li>Lore</li>
+	</ol>
+
+	<h2>Art Direction and References</h2>
+	<blockquote>
+		This section describes the visual and audio style of the game, including character and
+		environment design, animation style, color palette, user interface (UI) design, sound effects,
+		and music.
+	</blockquote>
+
+	<h3>Visual Style (details to be added later)</h3>
+	<ol>
+		<li>...</li>
+	</ol>
+
+	<h3>Character Design (details to be added later)</h3>
+
+	<h4>Reia Character Design Notes</h4>
+
+	<p>
+		<i>Reia is the main character of the game. She's one of the four recently created Ethereals.</i>
+	</p>
+
+	<p>
+		<b>Personality and Traits:</b>
+		Curious, brave, and empathetic. Reia has a strong sense of justice and a desire to help others. She
+		is also highly intelligent and resourceful. She's kind and compassionate to those who live the world
+		she created.
+	</p>
+
+	<p>
+		<b>Physical Appearance:</b>
+		Reia has a toned build with long, braided hair that glows with a soft, ethereal light. She has bright,
+		expressive eyes and a warm smile. Her clothing is practical yet stylish, designed for adventure.
+		She has a pendant on her sash near her hip. Inside of it is a star representing her symbol. This
+		same symbol appears 3 times on her necklace and once at the end of her hair braid.
+		<br />
+		Her outfit is a mid-length dress with a high slit on one side for mobility. The dress is adorned
+		with gold clips on the sides and she has a light shawl draped over her shoulders. Her dress also
+		curves and tapers off at the ends.
+		<br />
+		She wears wedges that lace up to her calves. The wedges have a slight heel for style but are still
+		practical. On the back of the wedges is her symbol along with some extra designs. The overall color
+		palette is white with a subtle mix of all of the elements (fire, water, earth, air, light, dark)
+		with an ombré design. And gold accents for jewelry.
+	</p>
+
+	<p>
+		<b>Abilities and Powers:</b>
+		Reia isn't particularly strong with any one speicific element. However, she has a natural affinity
+		for all elements, allowing her to adapt to various situations. Her mastery over using multiple abilities
+		allows her to combine elements in creative ways, making her strong in a unique way.
+	</p>
+
+	<h4>Urik Character Design Notes</h4>
+	<p>Soon...</p>
+
+	<h4>Tacitus Character Design Notes</h4>
+	<p>Soon...</p>
+
+	<h4>??? Character Design Notes</h4>
+	<p>Soon...</p>
+
+	<h3>Environment Design (details to be added later)</h3>
+	<ol>
+		<li>...</li>
+	</ol>
+
+	<h3>Animation Style (details to be added later)</h3>
+	<ol>
+		<li>...</li>
+	</ol>
+
+	<h3>Color Palette (details to be added later)</h3>
+	<ol>
+		<li>...</li>
+	</ol>
+
+	<h3>User Interface (UI) Design (details to be added later)</h3>
+	<ol>
+		<li>...</li>
+	</ol>
+
+	<h3>Audio Design (details to be added later)</h3>
+	<ol>
+		<li>...</li>
+	</ol>
+
+	<h3>Sound Effects (details to be added later)</h3>
+	<ol>
+		<li>...</li>
+	</ol>
+
+	<h3>Music (details to be added later)</h3>
+	<ol>
+		<li>...</li>
+	</ol>
+
+	<h2>Detailed Game Design</h2>
+	<blockquote>
+		This section dives deeper into the specific mechanics of the game, including combat, inventory,
+		crafting, player-owned islands, the infinite city, PvP, economy, and player-made content.
+	</blockquote>
+
+	<h3>Combat (details to be added later)</h3>
+	<ol>
+		<li>Spirits</li>
+		<li>Soulstones</li>
+		<li>Passive and Skill Tree</li>
+	</ol>
+
+	<h3>Inventory & Gear (details to be added later)</h3>
+	<ol>
+		<li>...</li>
+	</ol>
+
+	<h3>Crafting and Gathering (details to be added later)</h3>
+	<ol>
+		<li>...</li>
+	</ol>
+
+	<h3>Player-Owned Island (POI)</h3>
+	<ol>
+		<li>Players can customize their own island.</li>
+		<li>Build structures to harvest resources.</li>
+		<li>Hire NPCs or players to...</li>
+	</ol>
+
+	<h3>Infinite City</h3>
+	<ol>
+		<li>Dangers</li>
+		<li>Sharing or Selling Maps</li>
+		<li>Risks and Rewards</li>
+	</ol>
+
+	<h3>PvP (details to be added later)</h3>
+	<ol>
+		<li>Governining Cities and War</li>
+	</ol>
+
+	<h3>Economy (details to be added later)</h3>
+	<ol>
+		<li>...</li>
+	</ol>
+
+	<h3>Player-Made Content (details to be added later)</h3>
+	<ol>
+		<li>...</li>
+	</ol>
+
+	<h2>Technical Design</h2>
+	<blockquote>
+		This section outlines the technical aspects of the game's development, including the game
+		engine, graphics rendering, scripting languages, web programming, data handling, and
+		specifications and scope.
+	</blockquote>
+
+	<h3>Game Programming</h3>
+	<ol>
+		<li>Game Engine: The Godot Game Engine (v4.5+).</li>
+		<li>
+			Graphics Rendering: Vulkan & Direct3D 12 support for both high-performance graphics and old
+			GPUs. OpenGL ES 3.0 support web (HTML5) platforms and all companion releases.
+		</li>
+		<li>
+			Programming Languages: GDScript for high-level programming, Rust through gdext for more
+			performance.
+		</li>
+		<li>Shader Languages: GDShader/GLSL.</li>
+		<li>Localization: Godot's built-in translation system.</li>
+		<li>Networking: Sustenet, our custom networking solution built in Rust.</li>
+	</ol>
+
+	<h3>Web Programming</h3>
+	<ol>
+		<li>
+			Frontend: SvelteKit & Tailwind CSS for optimizations and SEO. Paraglide for i18n translations.
+		</li>
+		<li>
+			Backend: Vercel with Supabase for user authentication and storage. It'll also link with Turso
+			to load specific user data.
+		</li>
+	</ol>
+
+	<h3>Data Handling</h3>
+	<ol>
+		<li>
+			Authentication: Turso through Rust handles all OpenID Connect (OIDC) auths for Steam / Epic
+			Games / other platforms and also email / password signups. This gets linked to a Supabase
+			account to be used on the website.
+		</li>
+		<li>Online Database: Turso with Rust for authentication and core game data.</li>
+		<li>Offline Support: Turso with Rust enables seamless online and offline play.</li>
+	</ol>
+
+	<h3>Specs & Scope</h3>
+	<ol>
+		<li>
+			Target Platforms: PC, Mobile support, and a heavy focus on Console ports later. The companion
+			app will be a trimmed down 2D version of the game with a focus on crafting, gathering, and
+			social features. It'll also have combat, but it'll be more casual and less intensive than the
+			main game. This app is available on all platforms, including web (HTML5).
+		</li>
+		<li>Team Size: Core Team and community contributors.</li>
+		<li>
+			Project Timeline: We're aiming for an alpha release around May 2026. If it takes longer, we'll
+			postpone to reach our goal properly for the following year. But we want the release to be in
+			May for a Kickstarter.
+		</li>
+	</ol>
+
+	<h2>Production Design</h2>
+	<blockquote>
+		This section covers the plans for marketing, monetization, platform availability, funding, and
+		the overall production timeline.
+	</blockquote>
+
+	<ol>
+		<li>...</li>
+	</ol>
+
+	<h3>Marketing (details to be added later)</h3>
+	<ol>
+		<li>...</li>
+	</ol>
+
+	<h3>Monetization (details to be added later)</h3>
+	<ol>
+		<li>
+			How will the game make money? Premium purchase? F2P? How do you justify this within the
+			design?
+		</li>
+	</ol>
+
+	<p>
+		The game will be free to play on all platforms. However, there will be optional paid content
+		such as cosmetics, expansions, and a premium membership that offers quality of life features and
+		early access to new content. This approach allows us to keep the game accessible to everyone
+		while still generating revenue to support ongoing development and server costs. This content can
+		still be accessed without paying money. If a friend has the content unlocked, in most cases, you
+		can use it as well without paying.
+	</p>
+
+	<h3>Platforms</h3>
+	<ol>
+		<li>PC (Windows / Mac / Linux)</li>
+		<li>Mobile (iOS / Android)</li>
+		<li>Web (HTML5)</li>
+		<li>Console (PS5 / XBOX / Nintendo Switch)</li>
+	</ol>
+
+	<p>
+		The plan is to make the game accessible on multiple platforms, ensuring a wide reach and
+		engagement from various user bases. There's a strong focus on PC support. Mobile comes second,
+		and console comes third. The companion app is uniform across all platforms. The role of the
+		companion app is to allow players to engage with the game world in a more casual and social
+		manner, focusing on crafting, gathering, and light combat. It serves as a complement to the main
+		game, providing a different experience. You'll be able to manage things about your account and
+		character throug the companion app.
+	</p>
+
+	<h3>Funding (details to be added later)</h3>
+	<ol>
+		<li>
+			Self-funded: We went this route to stay true to the purpose of this project. To provide
+			something everyone loves without changing it up for profits. And if we run out of money,
+			that's okay. The project doesn't magically die.
+		</li>
+		<li>
+			Crowdfunded: We want to allow the community to support the game as well. Their support goes
+			straight into things that will obviously get a return. Payments to contributors through our
+			Open Collective and server costs as well.
+		</li>
+	</ol>
+
+	<h3>Production Plan (details to be added later)</h3>
+	<ol>
+		<li>...</li>
+	</ol>
+</article>
+
+<style lang="postcss">
+	@reference '$appcss';
+
+	article {
+		@apply mx-auto max-w-3xl px-4 pt-40 pb-8;
+	}
+
+	blockquote {
+		@apply border-r-gold my-4 border-l-4 pl-4 text-lg italic;
+	}
+	h2 {
+		@apply text-r-gold mt-10 text-3xl font-extrabold;
+	}
+	h3 {
+		@apply text-r-gold mt-8 text-2xl font-bold;
+	}
+	h4 {
+		@apply text-r-gold mt-6 text-xl font-bold;
+	}
+	ul,
+	ol {
+		@apply !list-inside;
+
+		li {
+			li {
+				@apply ml-6;
+			}
+		}
+	}
+	ol > li {
+		@apply !my-3 !list-decimal;
+	}
+	p {
+		@apply my-4;
+	}
+</style>
